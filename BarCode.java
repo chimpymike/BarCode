ae0d66c7bfb1dd8a27a06202efe28b8f9eb2456c
Mike Callahan <chimpymike@gmail.com>
2013-02-24 11:33:17 -0500

    Add the BarCode constructor to initialize the zipcode and barcode.

--- BarCode.java
+++ BarCode.java
@@ -1,4 +1,23 @@
 public class BarCode {
     private String myZipCode;
     private String myBarCode;
+
+    public BarCode(String code) {
+	// Determine if this is a zipcode or a barcode
+	// based on the length of the code string
+	if (code.length() == 5) {
+	    // This is a zipcode
+	    myZipCode = code;
+	    // Placeholder for the encode to barcode method
+	    // encode();
+	} else if (code.length() == 32) {
+	    // This is a barcode
+	    myBarCode = code;
+	    // Placeholder for the decode to zipcode method
+	    // decode()
+	} else {
+	    // This is not a valid zipcode or barcode
+	    System.out.println("Not a valid zipcode or barcode!!!");
+	}
+    }
 }
