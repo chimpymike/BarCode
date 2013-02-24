550295751e320c2e9a537cae9aa0635e26a5303b
Mike Callahan <chimpymike@gmail.com>
2013-02-24 11:49:28 -0500

    Instantiate a test barcode object with a zipcode and print the test barcode object's zipcode.

--- BarCodeTest.java
+++ BarCodeTest.java
@@ -1,5 +1,8 @@
 public class BarCodeTest {
     public static void main(String[] args) {
 	System.out.println("BarCodeTest");
+
+	BarCode testBarCode = new BarCode("02138");
+	System.out.println("ZipCode: " + testBarCode.getZipCode());
     }
 }
