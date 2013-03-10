06c21ecc964a9797dc5b3363209dbc9be4b5d7b4
Mike Callahan <chimpymike@gmail.com>
2013-03-10 16:59:57 -0400

    Add the code for parts 1 and 2 of the BarCodeTest class.

--- BarCodeTest.java
+++ BarCodeTest.java
@@ -2,7 +2,21 @@ public class BarCodeTest {
     public static void main(String[] args) {
 	System.out.println("BarCodeTest");
 
-	BarCode testBarCode = new BarCode("02138");
-	System.out.println("ZipCode: " + testBarCode.getZipCode());
+	// 1.
+	BarCode testBarCode1 = new BarCode("02138");
+	System.out.println("ZipCode: " + testBarCode1.getZipCode());
+	// System.out.println("Barcode: " + testBarCode.getZipCode());
+
+	// 2.
+	BarCode testBarCode2 = new BarCode("||:|:::|:|:||::::::||:|::|:::|||");
+	String testZipCode2 = testBarCode2.getZipCode();
+	if (testZipCode2.equals("")) {
+	    System.out.println("Error. " + testBarCode2 + " is not a valid BarCode.");
+	} else {
+	    System.out.println("ZipCode: + " testZipCode2);
+	}
+
+	
+	
     }
 }
